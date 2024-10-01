05fd8ca04354edeecb307eed2023d0004117167e
Jacob Samuel G. <26469677+tojacob@users.noreply.github.com>
2024-09-30 22:24:59 -0600

    add connect in package.json

--- connect/index.js
+++ connect/index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const app = connect();
 
 app.use('/', (req, res) => {
-  res.json({ status: 200 });
+  res.end(JSON.stringify({ status: 200 }));
 });
 
 http.createServer(app).listen(port, () => {
